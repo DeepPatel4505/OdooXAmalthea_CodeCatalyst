06cf3eb843dfd5b709c7f7d0f23347b8fa395b7d
krishna <jadejakrishnapal04@gmail.com>
2025-10-04 17:01:44 +0530

    dd

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,25 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   optimizeDeps: {
-    exclude: ['@reduxjs/toolkit']
+    exclude: ["@reduxjs/toolkit"],
   },
   build: {
     commonjsOptions: {
-      include: [/node_modules/]
-    }
-  }
-})
+      include: [/node_modules/],
+    },
+  },
+});
